badbc4357ef4cd3d1ace0c337c3e849db87aa96c
John Emory <jfemory@gmail.com>
2019-02-18 02:34:55 -0600

    commit

--- PlotDiffEQ.m
+++ PlotDiffEQ.m
@@ -1,7 +1,7 @@
 syms a t
 s = a*(50*t - 2500 + exp(-0.02*t)*2500)+exp(-0.02*t)*150-50;
 fs = fsurf(s, [0.0 0.02 0 180]);
-zlim([-20,110]);
+zlim([0,110]);
 title('Family of solutions to $\frac{ds}{dt}$','Interpreter','latex')
 xlabel('Acceleration $[ms^{-2}]$','Interpreter','latex')
 ylabel('Time $[s]$','Interpreter','latex')
